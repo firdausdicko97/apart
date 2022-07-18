06763fca3aebc63bb093e3156549b8cc922b82c0
firdausdicko97.com <firdausdicko@gmail.com>
2022-07-18 23:13:13 +0700

    first commit

---
+++ components/Navbar/index.vue
@@ -0,0 +1,32 @@
+<template>
+<header class="header">
+    <section class="navbar">
+
+        <div class="navicon">
+        <img src="~/assets/navicon.png" />
+        </div>
+
+        <section class="nav">
+            <a href="">Home</a>
+            <a href="">Residence</a>
+            <a href="">About Us</a>
+            <a href="">Agent</a>
+            <a href="">About you</a>
+            <button>login</button>
+        </section>
+
+    </section>
+</header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped>
+@import "~/static/css/navbar.css";
+@import url("https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200&display=swap");
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+// import Navbar from '../components/Navbar/index.vue';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+export default {
+  name: "IndexPage",
+  components: { Navbar, Footer },
+};
+</script>
