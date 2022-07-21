44116078d97374a4eb74e4b85b2ca7e5f2e25589
firdausdicko97 <firdausdicko@gmail.com>
2022-07-21 21:50:35 +0700

    add body

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,29 @@
   <body>
     <div class="container">
       <Navbar />
+      <!-- head -->
       <section class="head">
         <div class="gradient">
-          <img class="gradient1" src="~/assets/ellipse1.png" />
-          <img class="gradient2" src="~/assets/ellipse2.png" />
-          <img class="gradient3" src="~/assets/ellipse2.png" />
+          <img class="gradient" src="~/assets/gradient.png" />
+          <img class="apart1" src="~/assets/apart.png" />
         </div>
+        <section class="h1">
+          <section class="title">
+            <p>Find Your</p>
+            <p>Dream Home</p>
+          </section>
+          <section class="sub">
+            <p>
+              This is where you can find a dream home of your choice without
+              stress
+            </p>
+          </section>
+          <section class="search">
+            <p>Search the price you are looking for here</p>
+          </section>
+        </section>
       </section>
+      <!-- search -->
     </div>
   </body>
 </template>
@@ -25,4 +41,7 @@ export default {
 
 <style scoped>
 @import "~/static/css/home.css";
+@import url("https://fonts.googleapis.com/css?family=Montserrat");
+@import url("http://fonts.googleapis.com/css?family=Josefin+Sans&subset=latin,latin-ext");
+/* @import url("https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"); */
 </style>
